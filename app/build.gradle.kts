738ba8f9ba96bf646cdfc91165009e3435de4e82
JAIstudio-source <defender9817@gmail.com>
2026-09-26 14:10:57 +0530

    Release v3.1.0 (Build 33): fixed known issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         minSdk = 28
         targetSdk = 34
         
-        versionCode = 32
-        versionName = "3.0.0"
+        versionCode = 33
+        versionName = "3.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
